37d3e48194e36b32216c981528127135ee5a7447
MARIAPEDRERO <mariapedrerodelapuente@hotmail.com>
2017-08-18 16:54:31 -0400

    Finished mobile version V1. 2 languajes
    Not uploaded to server.
    Comments on JS functions

--- web_molino/JSMenu.js
+++ web_molino/JSMenu.js
@@ -3,7 +3,8 @@ $(document).ready(main);
 
 var estado = 1;
 
-
+// this function is used to control control the mobile version menu. (shows or not the menu items depending on the 'estado' var activation.)
+// 'estado' changes every time the nav menu icon is clicked or everytime a menu item is clicked
 function main () {
   $('.menu_bar').click(function(){
     if (estado == 1) {
@@ -38,9 +39,14 @@ function main () {
 
 }
 
+
+// this function is used to avoid overlap between the fixed menu and the #links. It offsets the anchor link 175 pixels. (menu width)
+//***********Warning!!!!!!!!!! menu width changes for index3.html, if deployed change the 175 value.
 function scrolling_function(){
   window.scrollTo(window.scrollX, window.scrollY - 175);
 }
 
+
+// call the scrolling_function everytime the hash changes (everytime a # link is clicked)
 window.onhashchange = scrolling_function;
 
